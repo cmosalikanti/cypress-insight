2b0714f5a2541c1db0273b47f9e8794962d2ac3c
Chandra Sekhar Mosalikanti <mcsekhar777@gmail.com>
2025-02-24 12:50:46 +0000

    improve test assertion

--- cypress/e2e/cypress-features/add-remove-element.spec.cy.ts
+++ cypress/e2e/cypress-features/add-remove-element.spec.cy.ts
@@ -1,10 +1,9 @@
 it('should be able to add an element', () => {
     cy.visit('/')
     cy.contains('Add/Remove Elements').click();
-    cy.should('have.text', 'Add/Remove Elements')
+    cy.get('#content h3').should('have.text', 'Add/Remove Elements')
     cy.get('button').click();
     cy.get('button.added-manually').should('be.visible')
-
     cy.get('button.added-manually').click();
     cy.get('button.added-manually').should('not.exist')
     cy.get('.example').get('button').should('be.visible')
